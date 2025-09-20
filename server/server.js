f8d0ff37633c008ab2459c4c327d4e4b61b968a5
Sunayana547 <sunayananeti12@gmail.com>
2025-09-20 15:24:11 +0530

    main

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mockData = require('./data/mockData');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5008;
 
 app.use(cors());
 app.use(express.json());
