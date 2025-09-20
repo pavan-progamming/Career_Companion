246a94fc0f6dc1c7a2ff55003939ddbef21f3f09
Sunayana547 <sunayananeti12@gmail.com>
2025-09-20 14:50:57 +0530

    h

--- client/src/apiService.js
+++ client/src/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // The live, public base URL for your deployed backend API
-export const API_BASE_URL = 'https://career-companion-smjj.onrender.com';
+export const API_BASE_URL = 'https://career-companion-server.onrender.com';
 
 // Create an Axios instance that will use this base URL for all requests
 const api = axios.create({
